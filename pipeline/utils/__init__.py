61561f9fde57026e25bd72c55ddc394059f9dc67
Dhruv <dhruvkapri2702@gmail.com>
2025-12-28 19:38:50 +0530

    feat: parameterize DVC pipeline using params.yaml

--- pipeline/utils/__init__.py
+++ pipeline/utils/__init__.py
@@ -2,6 +2,7 @@ from .logger import get_logger
 from .data import load_data, clean_schema, save_data
 from .model import load_model, save_model
 from .metrics import save_metrics
+from .params import load_params
 from .paths import (
     PROJECT_ROOT,
     DATA_DIR,
@@ -22,6 +23,7 @@ __all__ = [
     "load_model",
     "save_model",
     "save_metrics",
+    "load_params",
     "PROJECT_ROOT",
     "DATA_DIR",
     "RAW_DATA_DIR",
